06a87d69ff131aefd9d55a98bee573f427836a15
Kabilesh <kabilesh@dal.ca>
2024-02-15 17:16:28 -0400

    Updates

---
+++ precache-manifest.9223acb63c403d4d7dd3abe6b70d9570.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564c338ad2828c302756cac7cd74354c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71b9df67f7d609ff6a24",
+    "url": "/static/css/main.ef0e821b.chunk.css"
+  },
+  {
+    "revision": "034170faeb97c2fe3e62",
+    "url": "/static/js/2.254920a3.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.254920a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b9df67f7d609ff6a24",
+    "url": "/static/js/main.65603fe9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "cbd8551efceb0b90222a77b70b909ec2",
+    "url": "/static/media/Dp.cbd8551e.JPG"
+  },
+  {
+    "revision": "609fff0644cd5b850517753301535e3a",
+    "url": "/static/media/KabileshResumee.609fff06.pdf"
+  }
+]);
